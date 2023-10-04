bdd1c95273037df3f29ee8133b160a59de75e0dd
mohmmad Smadi <mohmmadmms20@gmail.com>
2023-10-05 00:54:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(arr, name) {
+    return arr.filter((el) => el.toLowerCase().includes(name.toLowerCase()));
+  }
+
+  function fuzzyMatch(arr, name) {
+    
+    const lowercaseQuery = name.toLowerCase();
+  
+    
+    const matchingDrivers = arr.filter(driver => driver.toLowerCase().startsWith(lowercaseQuery));
+  
+    return matchingDrivers;
+  }
+
+  function  matchName(driver , names){
+    const matchingDrivers =driver.filter(n => n.name.toLowerCase()===names.toLowerCase())
+    return matchingDrivers
+
+  }
+
